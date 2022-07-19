1674da8aa9bbebc357c65bad2c2a306f710425f1
Albert <albert1999dual@gmail.com>
2022-07-19 14:54:16 +0200

    añadida la url de captura de la nueva camara IP

--- img_capture.py
+++ img_capture.py
@@ -8,10 +8,11 @@ def captura_imagen():
     # http://192.168.1.108/CGI/command/snap?channel=0
     # aqui pedimos captura a la camara por red.
     try:
-        r = requests.get('http://192.168.1.108/CGI/command/snap?channel=0')
+        r = requests.get('http://192.168.1.71:6688/snapshot/PROFILE_000')
         file = open("unprocessed_imgs/captura_camara.jpeg", "wb")
         file.write(r.content)
         file.close()
         upload.upload('unprocessed_imgs/captura_camara.jpeg')
     except Exception as e:
         print("error en la captura")
+# captura_imagen()
